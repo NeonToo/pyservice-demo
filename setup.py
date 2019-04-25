28ff26d2f2ef2530af88c22cb8ec730c47dbe967
NeonToo <469565714@qq.com>
2019-04-25 11:09:35 +0800

    add setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyservice-demo',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/NeonToo/pyservice-demo.git',
+    license='',
+    author='kaydu',
+    author_email='',
+    description='Miscroservice demo of Python.'
+)
