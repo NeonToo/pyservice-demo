1292a3e54f18c8687dc8beed031a4de72e8e27d2
NeonToo <469565714@qq.com>
2019-04-25 15:31:26 +0800

    add requirement.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 from setuptools import setup, find_packages
+import os
+import sys
+import codecs
+
+
+def read_file(filename):
+    """Read and return `filename` in root dir of project and return string ."""
+    here = os.path.abspath(os.path.dirname(__file__))
+    return codecs.open(os.path.join(here, filename), 'r').read()
+
+
+install_requires = []
+if os.path.exists("requirement.txt"):
+    install_requires = read_file("requirement.txt").split()
+    if "win" in sys.platform:
+        for item in ['pexpect']:
+            try:
+                install_requires.remove(item)
+            except ValueError as e:
+                pass
 
 setup(
     name='pyservice-demo',
@@ -9,5 +29,6 @@ setup(
     author='kaydu',
     description='Microservice demo of Python.',
     include_package_data=True,
-    exclude_package_date={'': ['.gitignore']},
+    exclude_package_data={'': ['.gitignore']},
+    install_requires=install_requires
 )
